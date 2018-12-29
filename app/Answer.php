6d8567e312921d31894e0bcabeaf3f8d91010224
Rehmatullah Khan <rehmat_leo@yahoo.com>
2018-12-29 09:14:06 +0500

    some unsaved changes

--- app/Answer.php
+++ app/Answer.php
@@ -14,4 +14,8 @@ class Answer extends Model
    {
        return $this->belongsTo(User::class);
    }
+   public function getBodyHtmlAttribute()
+   {
+       return \Parsedown::instance()->text($this->body);
+   }
 }
